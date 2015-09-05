6cc3cf9ec6b3765688ea5ee833305c361a8581e8
joaobiriba <glaquidara@gmail.com>
2015-09-05 16:06:26 +0200

    A11Y

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -193,8 +193,7 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
             mFriendlyDateView.setText(friendlyDateText);
             mDateView.setText(dateText);
 
-            // Read description from cursor and update view
-            String description = data.getString(COL_WEATHER_DESC);
+            String description = Utility.getStringForWeatherCondition(getActivity(), weatherId);
             mDescriptionView.setText(description);
 
             // For accessibility, add a content description to the icon field
